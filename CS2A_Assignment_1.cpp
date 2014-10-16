0a2652ce835e56d98cb3af2723fcd295b38b9cb4
Sergiogitggi <sedelstein@hotmail.com>
2014-10-16 16:56:42 -0700

    The original commit for CS 2A Assignment 1

---
+++ CS2A_Assignment_1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int  main()
+{
+   cout << "Hello, Mr. Loceff:\n\n";
+   cout << "My family name is Edelstein." << endl;
+   cout << "My Foothill student ID is 20220296." << endl << endl;
+   cout << "Guidelines paraphrased from CS-2A Syllabus:" << endl << endl;
+   cout << "Page #1 Detail #1: "
+        << "A particular textbook is recommended, but not required." << endl;
+   cout << "Page #1 Detail #2: "
+        << "Code we write for assignments should not be posted on" << endl;
+   cout << "                   Public Discussion area." << endl;
+   cout << "Page #2 Detail #1: "
+        << "We will take a midterm exam on Friday of the sixth week." << endl;
+   cout << "Page #2 Detail #2: "
+        << "Late assignments will not be accepted after December 5." << endl << endl;
+   return 0;
+}
+
+/*  ----------------------  paste of run  --------------------------------
+
+Hello, Mr. Loceff:
+
+My family name is Edelstein.
+My Foothill student ID is 20220296.
+
+Guidelines paraphrased from CS-2A Syllabus:
+
+Page #1 Detail #1: A particular textbook is recommended, but not required.
+Page #1 Detail #2: Code we write for assignments should not be posted on
+                   Public Discussion area.
+Page #2 Detail #1: We will take a midterm exam on Friday of the sixth week.
+Page #2 Detail #2: Late assignments will not be accepted after December 5.
+
+Press any key to continue . . .
+
+-------------------------------------------------------------------------- */
